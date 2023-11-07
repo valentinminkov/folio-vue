137bf4eed8e3d28618d4c497829b8da0d6cf9ed1
minkowalentin <minkowalentin@proton.me>
2023-11-07 10:05:27 +0100

    add basic button component

---
+++ src/components/BasicButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { type Button } from '@/types/components'
+const { text, url } = defineProps<Button>()
+</script>
+
+<template>
+  <a :href="url" class="button">{{ text }}</a>
+</template>
