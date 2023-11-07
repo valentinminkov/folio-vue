8df9128bda46e651c2471eeafd736f741d926990
minkowalentin <minkowalentin@proton.me>
2023-11-07 10:05:57 +0100

    add basic heroBlock component

---
+++ src/components/HeroBlock.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import BasicButton from '@/components/BasicButton.vue'
+import { type HeroBlock } from '@/types/components'
+
+const { title, subtitle, button } = defineProps<HeroBlock>()
+</script>
+
+<template>
+  <div class="heroContainer">
+    <h1 class="title">{{ title }}</h1>
+    <h3 v-if="subtitle" class="subtitle">{{ subtitle }}</h3>
+    <div v-if="button && button.text && button.url" class="buttonContainer">
+      <BasicButton :url="button.url" :text="button.text" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.heroContainer {
+  @include baseContainer;
+  background-color: $base_color;
+  .title {
+    @include h1;
+  }
+  .subtitle {
+    @include h3;
+  }
+}
+</style>
